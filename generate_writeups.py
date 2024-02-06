8dbdea7bbf6f87dff8e7ee1dbdda45de55cd1cf8
BIGJRA <47573834+BIGJRA@users.noreply.github.com>
2024-02-06 12:08:51 -0500

    minor

--- generate_writeups.py
+++ generate_writeups.py
@@ -75,28 +75,32 @@ TUTOR_SCORES = {
     "Swagger": 20  # Slateport
 }
 
+
 def generate_text(pokemon_names):
     lines = []
     first_mon = pokemon_names[0].capitalize()
     lines.append(f"# Pokemon Emerald Unit Feel Analysis: {first_mon} line\n")
-    lines.append(f"{first_mon} is first obtained at TODO. It's ability is TODO. It evolves at TODO, then again at TODO.\n")
+    lines.append(f"{first_mon} is first obtained at TODO. It's ability is TODO. It evolves at TODO, then again at "
+                 f"TODO.\n")
     lines.append('### Base Stats\n')
     for mon in pokemon_names:
         j = get_json(mon)["stats"]
-        lines.append(f"{mon.capitalize()}: **{j["HP"]}** HP / **{j["Attack"]}** Atk / **{j["Defense"]}** Def / **{j["Sp. Atk"]}** SpA / **{j["Sp. Def"]}** SpD / **{j["Speed"]}** Spe\n")
+        lines.append(
+            f"{mon.capitalize()}: **{j["HP"]}** HP / **{j["Attack"]}** Atk / **{j["Defense"]}** Def / **{j["Sp. Atk"]}** SpA / **{j["Sp. Def"]}** SpD / **{j["Speed"]}** Spe\n")
 
     lines.append('### Moveset\n')
 
     acq, data = get_learnset_and_move_data(pokemon_names)
 
-    move_lines = [['Acquisition','---'], ['Move','---'], ['Type','---'], ['Power','---'], ['Accuracy','---'], ['PP','---'],['Notes'.ljust(25), '---']]
+    move_lines = [['Acquisition', '---'], ['Move', '---'], ['Type', '---'], ['Power', '---'], ['Accuracy', '---'],
+                  ['PP', '---'], ['Notes'.ljust(25), '---']]
     for move in acq:
         n = move[1]
         type, pwr, acc, pp = data[n]["Type"], data[n]["Pwr"], data[n]["Acc"], data[n]["PP"]
         notes = ''
         if 'Tutor' in move[2]: notes = "Emerald only"
         for idx, thing in enumerate([f'{move[2]}', f'{n}', f'{type}', f'{pwr}', f'{acc}', f'{pp}', f'{notes}']):
-           move_lines[idx].append(thing)
+            move_lines[idx].append(thing)
     lengths = [max([len(s) for s in group]) for group in move_lines]
     for pos in range(len(move_lines[0])):
         curr = "|" + "|".join([thing[pos].ljust(lengths[idx]) for idx, thing in enumerate(move_lines)]) + "|"
@@ -105,6 +109,7 @@ def generate_text(pokemon_names):
     lines.append('')
     lines.append("### Analysis")
     return '\n'.join(lines)
+
 
 def get_learnset_and_move_data(pokemon_names):
     acq = defaultdict(lambda: [None for _ in range(len(pokemon_names))])
@@ -118,7 +123,7 @@ def get_learnset_and_move_data(pokemon_names):
             if m["LearnType"] == "LevelingUp":
                 try:
                     acq[(m["Move"], m["LearnType"])][curr] = (int(m["Level"]), f"Lv. {m["Level"]}")
-                except KeyError: # Some movesets are constructed with "RSE" as the key instead of "Level"
+                except KeyError:  # Some movesets are constructed with "RSE" as the key instead of "Level"
                     if m["RSE"] == "N/A": continue
                     acq[(m["Move"], m["LearnType"])][curr] = (int(m["RSE"]), f"Lv. {m["RSE"]}")
             elif m["LearnType"] == "TmHm":
@@ -130,7 +135,8 @@ def get_learnset_and_move_data(pokemon_names):
     for a in acq:
         move, _learntype = a
         entries = acq[a]
-        if (len(entries) == 3 and entries[0] == entries[1] == entries[2]) or (len(entries) == 2 and entries[0] == entries[1]):
+        if (len(entries) == 3 and entries[0] == entries[1] == entries[2]) or (
+                len(entries) == 2 and entries[0] == entries[1]):
             learn_str = entries[0][1]
         else:
             learn_str = ' / '.join([e[1] if e is not None else '--' for e in entries])
@@ -144,10 +150,12 @@ def get_learnset_and_move_data(pokemon_names):
     acq_list.sort()
     return acq_list, move_data
 
+
 def get_json(pokemon_name):
     with open(path.join('data', f'{pokemon_name}_3.json')) as f:
         j = json.loads(f.read())
     return j
+
 
 def write_analysis(text, pokemon_names):
     hi = pokemon_names[-1].lower()
@@ -158,7 +166,7 @@ def write_analysis(text, pokemon_names):
 if __name__ == "__main__":
     with open('lines.txt', 'r') as f:
         for line in f.readlines():
-            mons = line.replace('\n','').split(',')
+            mons = line.replace('\n', '').split(',')
             print(f"Creating template for {mons[-1]} line...")
             t = generate_text(mons)
             write_analysis(t, mons)
